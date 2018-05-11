590f84d24dfd918b6f3b49f1d6653c3a96232863
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-11 21:01:14 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -15,15 +15,15 @@ class MusicLibraryController
     until input == "exit"
 
       puts
-      puts "To list all of your songs, enter 'list songs'."
-      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of your SHITTY songs, enter 'list songs'."
+      puts "To list all of the SHITTY artists in your library, enter 'list artists'."
       puts "To list all of the genres in your library, enter 'list genres'."
       puts "To list all of the songs by a particular artist, enter 'list artist'."
       puts "To list all of the songs of a particular genre, enter 'list genre'."
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts
-      puts "What would you like to do?"
+      puts "What the fuck would you like to do? huh??"
 
       input = gets.strip
       case input
